29fcb661dd607e4822437b501f9ea1677cdd1923
dani0004 <dani0004@algonquinlive.com>
2013-12-21 07:30:22 -0500

    Initial Commit

---
+++ CriticalInfrastructureFramework/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CriticalInfrastructureFramework
+//
+//  Created by Wendy-Anne Daniel on 2013-12-21.
+//  Copyright (c) 2013 Wendy-Anne Daniel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CriticalInfrastructureFramework/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CriticalInfrastructureFramework
+//
+//  Created by Wendy-Anne Daniel on 2013-12-21.
+//  Copyright (c) 2013 Wendy-Anne Daniel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
